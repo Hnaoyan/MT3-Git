10d0031056bdf01013deddba9cf297774b421ba5
Hnaoyan <k022g00421@g.neec.ac.jp>
2023-04-13 23:37:54 +0900

    00_01完了

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <Novice.h>
+#include"Calc.h"
 
 const char kWindowTitle[] = "学籍番号";
 
@@ -21,9 +22,25 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		memcpy(preKeys, keys, 256);
 		Novice::GetHitKeyStateAll(keys);
 
+		Vector3 v1 = { 1.0f,3.0f,-5.0f };
+		Vector3 v2 = { 4.0f,-1.0f,2.0f };
+		float k = { 4.0f };
+
+		Calc* calc = new Calc();
+
+		Vector3 resultAdd = calc->Add(v1, v2);
+		Vector3 resultSubtract = calc->Subtract(v1, v2);
+		Vector3 resultMultiply = calc->Multiply(k, v1);
+		float resultDot = calc->Dot(v1, v2);
+		float resultLength = calc->Length(v1);
+		Vector3 resultNormalize = calc->Normalize(v2);
+
+
 		///
 		/// ↓更新処理ここから
 		///
+
+		float kRowHeight = 30;
 
 		///
 		/// ↑更新処理ここまで
@@ -32,6 +49,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		calc->VectorScreenPrintf(0, 0, resultAdd, " : Add");
+		calc->VectorScreenPrintf(0, int(kRowHeight), resultSubtract, " : Subtract");
+		calc->VectorScreenPrintf(0, int(kRowHeight) * 2, resultMultiply, " : Multiply");
+		Novice::ScreenPrintf(0, int(kRowHeight) * 3, "%.02f : Dot", resultDot);
+		Novice::ScreenPrintf(0, int(kRowHeight) * 4, "%.02f : Length", resultLength);
+		calc->VectorScreenPrintf(0, int(kRowHeight) * 5, resultNormalize, " : Normalize");
+
 
 		///
 		/// ↑描画処理ここまで
