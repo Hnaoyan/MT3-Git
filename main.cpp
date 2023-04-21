cd0a6eaf97f16f622a6987e26c0544c0b946a714
Hnaoyan <k022g00421@g.neec.ac.jp>
2023-04-21 11:02:34 +0900

    必要のない部分削除

--- main.cpp
+++ main.cpp
@@ -26,32 +26,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
 
-	Matrix4x4 m1 =
-	{
-		3.2f,0.7f,9.6f,4.4f,
-		5.5f,1.3f,7.8f,2.1f,
-		6.9f,8.0f,2.6f,1.0f,
-		0.5f,7.2f,5.1f,3.3f
-	};
+	Matrix* matrix = nullptr;
 
-	Matrix4x4 m2 =
-	{
-		4.1f,6.5f,3.3f,2.2f,
-		8.8f,0.6f,9.9f,7.7f,
-		1.1f,5.5f,6.6f,0.0f,
-		3.3f,9.9f,8.8f,2.2f
-	};
-
-	Matrix* matrix = new Matrix();
-
-	Matrix4x4 resultAdd = matrix->Add(m1, m2);// 加法
-	Matrix4x4 resultMultiply = matrix->Multiply(m1, m2);
-	Matrix4x4 resultSubtract = matrix->Subtract(m1, m2);
-	Matrix4x4 inverseM1 = matrix->Inverse(m1);
-	Matrix4x4 inverseM2 = matrix->Inverse(m2);
-	Matrix4x4 transposeM1 = matrix->Transpose(m1);
-	Matrix4x4 transposeM2 = matrix->Transpose(m2);
-	Matrix4x4 identity = matrix->MakeIdentity4x4();
+	//Matrix4x4 worldMatrix=
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -74,15 +51,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-		MatrixScreenPrintf(int(0), int(0), resultAdd, "Add");
-		MatrixScreenPrintf(int(0), int(kRowHeight * 5), resultSubtract, "Subtract");
-		MatrixScreenPrintf(int(0), int(kRowHeight * 5 * 2), resultMultiply, "Multiply");
-		MatrixScreenPrintf(int(0), int(kRowHeight * 5 * 3), inverseM1, "inverseM1");
-		MatrixScreenPrintf(int(0), int(kRowHeight * 5 * 4), inverseM2, "inverseM2");
-		MatrixScreenPrintf(int(kColumnWidth * 5), 0, transposeM1, "transposeM1");
-		MatrixScreenPrintf(int(kColumnWidth * 5), int(kRowHeight * 5), transposeM2, "transposeM2");
-		MatrixScreenPrintf(int(kColumnWidth * 5), int(kRowHeight * 5 * 2), identity, "identity");
-
 
 		///
 		/// ↑描画処理ここまで
@@ -93,7 +61,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 		// ESCキーが押されたらループを抜ける
 		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
-			delete matrix;
 			break;
 		}
 	}
