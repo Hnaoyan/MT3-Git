34e52d8022f660249bb6499446957fc9c1bd7b70
Hnaoyan <k022g00421@g.neec.ac.jp>
2023-04-21 13:04:43 +0900

    途中

--- Render.h
+++ Render.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Matrix4x4.h"
+#include "Vector3.h"
 class Render
 {
 public:
@@ -10,5 +11,7 @@ public:
 	Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip);
 	// 3.Viewport
 	Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth);
+	// 4.Cross
+	Vector3 Cross(const Vector3& v1, const Vector3& v2);
 };
 
--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <Novice.h>
 #include"Matrix.h"
 #include "Render.h"
+#include <stdint.h>
 
 const char kWindowTitle[] = "学籍番号";
+const int kWindowWidth = 1280;
+const int kWindowHeight = 720;
 const int kColumnWidth = 60;
 const int kRowHeight = 20;
 
@@ -17,41 +20,32 @@ void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label
 	}
 }
 
-static const int kRowHeight = 20;
-static const int kColumnWidth = 60;
-
-void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label) {
-	Novice::ScreenPrintf(x, y, "%s", label);
-	for (int row = 0; row < 4; ++row) {
-		for (int column = 0; column < 4; ++column) {
-			Novice::ScreenPrintf(
-				int(x + column * kColumnWidth), int(y + (row + 1) * kRowHeight), "%6.02f", matrix.m[row][column]
-			);
-		}
-	}
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
+	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vector.y);
+	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", vector.z);
+	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
 }
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
-	Novice::Initialize(kWindowTitle, 1280, 720);
+	Novice::Initialize(kWindowTitle, kWindowWidth, kWindowHeight);
 
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
 
 	Matrix* matrix = nullptr;
-
-	//Matrix4x4 worldMatrix=
-
 	Render* render = nullptr;
 
-	Matrix4x4 orthographicMatrix =
-		render->MakeOrthographicMatrix(-160.0f, 160.0f, 200.0f, 300.0f, 0.0f, 1000.0f);
-	Matrix4x4 perspectiveFovMatrix = 
-		render->MakePerspectiveFovMatrix(0.63f, 1.33f, 0.1f, 1000.0f);
-	Matrix4x4 viewportMatrix =
-		render->MakeViewportMatrix(100.0f, 200.0f, 600.0f, 300.0f, 0.0f, 1.0f);
+	Vector3 v1{ 1.2f,-3.9f,2.5f };
+	Vector3 v2{ 2.8f,0.4f,-1.3f };
+	Vector3 rotate{};
+	Vector3 translate{};
+	Vector3 cameraPosition{};
+
+	Vector3 cross = render->Cross(v1, v2);
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -66,6 +60,26 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		if (keys[DIK_W]) {
+			cameraPosition.y += 1;
+		}
+		else if (keys[DIK_S]) {
+			cameraPosition.y -= 1;
+		}
+
+		// 各種計算
+		Matrix4x4 worldMatrix = matrix->MakeAffineMatrix({ 1.0f,1.0f,1.0f }, rotate, translate);
+		Matrix4x4 cameraMatrix = matrix->MakeAffineMatrix({ 1.0f,1.0f,1.0f }, { 0.0f,0.0f,0.0f }, cameraPosition);
+		Matrix4x4 viewMatrix = matrix->Inverse(cameraMatrix);
+		Matrix4x4 projectionMatrix = render->MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
+		Matrix4x4 worldViewProjectionMatrix = matrix->Multiply(worldMatrix, matrix->Multiply(viewMatrix, projectionMatrix));
+		Matrix4x4 viewportMatrix = render->MakeViewportMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
+		Vector3 screenVertices[3];
+		for (uint32_t i = 0; i < 3; ++i) {
+			Vector3 ndcVertex=matrix->Transform()
+		}
+
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -73,6 +87,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		VectorScreenPrintf(0, 0, cross, "Cross");
 
 
 		///
