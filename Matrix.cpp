aa95ca3a37cd80859b41420ccb3b9c12ead7fde7
Hnaoyan <k022g00421@g.neec.ac.jp>
2023-04-21 11:10:16 +0900

    行列関数追加

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,4 +1,6 @@
 #include "Matrix.h"
+#include<assert.h>
+#include <cmath>
 
 Matrix4x4 Matrix::Add(const Matrix4x4& m1, const Matrix4x4& m2) {
 	Matrix4x4 result;
@@ -148,3 +150,83 @@ Matrix4x4 Matrix::MakeIdentity4x4() {
 	return result;
 }
 
+Matrix4x4 Matrix::MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate) {
+	Matrix4x4 result = {};
+	Matrix4x4 matrixX = MakeRotateXMatrix(rotate.x);
+	Matrix4x4 matrixY = MakeRotateYMatrix(rotate.y);
+	Matrix4x4 matrixZ = MakeRotateZMatrix(rotate.z);
+	Matrix4x4 matrixRotate = Multiply(matrixX, Multiply(matrixY, matrixZ));
+
+	result.m[0][0] = scale.x * matrixRotate.m[0][0];
+	result.m[0][1] = scale.x * matrixRotate.m[0][1];
+	result.m[0][2] = scale.x * matrixRotate.m[0][2];
+	result.m[1][0] = scale.y * matrixRotate.m[1][0];
+	result.m[1][1] = scale.y * matrixRotate.m[1][1];
+	result.m[1][2] = scale.y * matrixRotate.m[1][2];
+	result.m[2][0] = scale.z * matrixRotate.m[2][0];
+	result.m[2][1] = scale.z * matrixRotate.m[2][1];
+	result.m[2][2] = scale.z * matrixRotate.m[2][2];
+
+	result.m[3][3] = 1.0f;
+	result.m[3][0] = translate.x;
+	result.m[3][1] = translate.y;
+	result.m[3][2] = translate.z;
+
+	return result;
+}
+
+Matrix4x4 Matrix::MakeRotateXMatrix(float radian) {
+	Matrix4x4 result = {};
+	result.m[0][0] = 1.0f;
+	result.m[1][1] = std::cosf(radian);
+	result.m[1][2] = std::sinf(radian);
+	result.m[2][1] = std::sinf(-radian);
+	result.m[2][2] = std::cosf(radian);
+	result.m[3][3] = 1.0f;
+	return result;
+}
+
+Matrix4x4 Matrix::MakeRotateYMatrix(float radian) {
+	Matrix4x4 result = {};
+	result.m[0][0] = std::cosf(radian);
+	result.m[0][2] = std::sinf(-radian);
+	result.m[1][1] = 1.0f;
+	result.m[2][0] = std::sinf(radian);
+	result.m[2][2] = std::cosf(radian);
+	result.m[3][3] = 1.0f;
+	return result;
+}
+
+Matrix4x4 Matrix::MakeRotateZMatrix(float radian) {
+	Matrix4x4 result = {};
+	result.m[0][0] = std::cosf(radian);
+	result.m[0][1] = std::sinf(radian);
+	result.m[1][0] = std::sinf(-radian);
+	result.m[1][1] = std::cosf(radian);
+	result.m[2][2] = 1.0f;
+	result.m[3][3] = 1.0f;
+	return result;
+}
+
+Vector3 Matrix::Multiply(float scaler, const Vector3& v) {
+	Vector3 result;
+
+	result.x = scaler * v.x;
+	result.y = scaler * v.y;
+	result.z = scaler * v.z;
+
+	return result;
+}
+
+Vector3 Matrix::Transform(const Vector3& vector, const Matrix4x4& matrix) {
+	Vector3 result = {};
+	result.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[1][0] + vector.z * matrix.m[2][0] + 1.0f * matrix.m[3][0];
+	result.y = vector.x * matrix.m[0][1] + vector.y * matrix.m[1][1] + vector.z * matrix.m[2][1] + 1.0f * matrix.m[3][1];
+	result.z = vector.x * matrix.m[0][2] + vector.y * matrix.m[1][2] + vector.z * matrix.m[2][2] + 1.0f * matrix.m[3][2];
+	float w = vector.x * matrix.m[0][3] + vector.y * matrix.m[1][3] + vector.z * matrix.m[2][3] + 1.0f * matrix.m[3][3];
+	assert(w != 0.0f);
+	result.x /= w;
+	result.y /= w;
+	result.z /= w;
+	return result;
+}
--- Matrix.h
+++ Matrix.h
@@ -1,7 +1,7 @@
 #pragma once
-struct Matrix4x4 {
-	float m[4][4];
-};
+#include "Matrix4x4.h"
+#include "Vector3.h"
+
 class Matrix
 {
 public:
@@ -11,10 +11,18 @@ public:
 public:
 	Matrix4x4 Add(const Matrix4x4& m1, const Matrix4x4& m2);
 	Matrix4x4 Subtract(const Matrix4x4& m1, const Matrix4x4& m2);
-	Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
 	Matrix4x4 Inverse(const Matrix4x4& m);
 	Matrix4x4 Transpose(const Matrix4x4& m);
 	Matrix4x4 MakeIdentity4x4();
+	Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
+	Matrix4x4 MakeRotateXMatrix(float radian);
+	Matrix4x4 MakeRotateYMatrix(float radian);
+	Matrix4x4 MakeRotateZMatrix(float radian);
+	Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
+	Vector3 Multiply(float scaler, const Vector3& v);
+	Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix);
+
+
 private:
 	static const int IndexX = 4;
 	static const int IndexY = 4;
